abf074924f86943547f31005febba94f38622fa4
kinoko Matumoto <superkinoko@icloud.com>
2020-04-06 11:29:06 +0900

    Rename env sample

--- src/environments/environment.sample.ts
+++ src/environments/environment.sample.ts
@@ -1,8 +1,8 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environmentSample.sample.ts` with `environmentSample.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+export const environmentSample = {
   production: false,
   docsBasePath: '<basepath-to-your-docs>'
 };
